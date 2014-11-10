746e8bca13a25cc7c842e2ffc5b1a02a1437b58a
parkus <parke.loyd@gmail.com>
2014-11-10 10:24:49 -0700

    checkpt

--- organize.py
+++ organize.py
@@ -19,46 +19,49 @@ def auto_rename(folder):
     tele = None
     unchanged = []
     for name in names:
-#        try:
-        filepath = os.path.join(folder, name)
-        hdr = fits.getheader(filepath)
-        
-        telekeys = ['telescop']
-        for telekey in telekeys:
-            try:
-                tele = hdr[telekey]
-            except:
+        try:
+            filepath = os.path.join(folder, name)
+            hdr = fits.getheader(filepath)
+            
+            telekeys = ['telescop']
+            for telekey in telekeys:
+                try:
+                    tele = hdr[telekey]
+                except:
+                    tele = None
+            
+            if tele is None:
+                unchanged.append(name)
                 continue
-        
-        if tele is None:
+            if tele == 'HST':
+                #using the x1d to get the appropriate info, rename all the files
+                #from the same observation
+                try:
+                    root = hdr['rootname']
+                except KeyError:
+                    root = name[-18:-9]
+                x1dfile = filter(lambda s: (root + '_x1d.fits') in s, names)[0]
+                xpath = os.path.join(folder,x1dfile)
+                xhdr = fits.getheader(xpath)
+                inst = xhdr['instrume']
+                if inst == 'STIS': inst = 'STS'
+                grating = xhdr['opt_elem']
+                target = xhdr['targname']
+                cenwave = xhdr['cenwave']
+                band = 'U' if cenwave < 4000.0 else 'V'
+                
+                obsnames = filter(lambda s: root in s, names)
+                for oname in obsnames:
+                    names.remove(oname)
+                    opath = os.path.join(folder, oname)
+                    original_name = fits.getval(opath, 'filename')
+                    newname = '_'.join([band, tele, inst, grating, target, 
+                                        original_name])
+                    os.rename(opath, os.path.join(folder, newname))           
+        except:
             unchanged.append(name)
             continue
-        if tele == 'HST':
-            #using the x1d to get the appropriate info, rename all the files
-            #from the same observation
-            root = name[:9]
-            x1dfile = filter(lambda s: (root + '_x1d.fits') in s, names)[0]
-            xpath = os.path.join(folder,x1dfile)
-            xhdr = fits.getheader(xpath)
-            inst = xhdr['instrume']
-            if inst == 'STIS': inst = 'STS'
-            grating = xhdr['opt_elem']
-            target = xhdr['targname']
-            cenwave = xhdr['cenwave']
-            band = 'U' if cenwave < 4000.0 else 'V'
-            
-            obsnames = filter(lambda s: s.count(root), names)
-            for oname in obsnames:
-                names.remove(oname)
-                opath = os.path.join(folder, oname)
-                original_name = fits.getval(opath, 'filename')
-                newname = '_'.join([band, tele, inst, grating, target, 
-                                    original_name])
-                os.rename(opath, os.path.join(folder, newname))
-                    
-#        except:
-#            unchanged.append(name)
-#            continue
+        
     if len(unchanged) > 0:
         print 'The following files could not be renamed:'
         for name in unchanged: print '    ' + name
--- reduce.py
+++ reduce.py
@@ -11,35 +11,75 @@ from astropy.table import Table, vstack
 import warnings
 import my_numpy as mnp
 import specutils
+import os.path as path
 
-def cat_specs(specfiles):
+def panspectrum(specfiles, R=1000.0):
     """
-    Concatenate spectra contained in FITS files.
+    Coadd and splice the provided spectra into one panchromatic spectrum
+    sampled at the native resolutions and constant R.
+    """
+    stars = [path.basename(s)[16:21] for s in specfiles]
+    star = np.unique(stars)
+    if len(star) > 1:
+        raise ValueError('More than one target in the provided spectra.')
+    star = star[0]
+        
+    #coadd all spectra from the same configuration
+    allconfigs = [path.basename(s)[:15] for s in specfiles]
+    configs = np.unique(allconfigs)
+    coadds = []
+    for config in configs:
+        cfiles = filter(lambda s: config in s, specfiles)
+        if len(cfiles) == 1:
+            spectbls = fits2tbl(cfiles[0])
+            coadds.extend(spectbls)
+        else:
+            spectbls = map(fits2tbl, cfiles)
+            spectbls = zip(*spectbls)
+            ccoadds = map(coadd, spectbls)
+            coadds.append(ccoadds)
+    
+    #normalize the spectra
+    
+    
+    #concatenate the coadded spectra
+    catspec = concatenate(coadds)
+    
+    #resample at constant R
+    Rspec = powerbin(catspec, R)
+    
+    return catspec,Rspec
+    
+    
+def concatenate(spectra):
+    """
+    Concatenate spectra contained in tables or FITS files.
     
     Where spectra overlap, uses the one with the higher overall S/N.
     """
     
-    #use my file naming convention to determine the configuration
-    configs = [s[3:16] for s in specfiles]
-    if len(np.unique(configs)) < configs:
-        warnings.warn('Two spectra from the same configurations were input. '
-                      'This function will not coadd them. You may want to do '
-                      'that before feeding them in here.')
-    
-    #read in all of the spectra and sort them by starting wavelength
-    spectra = []
-    for sf in specfiles:
-        spectra.extend(spectbl(sf))
+    if type(spectra) is str:
+        #use my file naming convention to determine the configuration
+        configs = [path.basename(s)[3:16] for s in spectra]
+        if len(np.unique(configs)) < configs:
+            warnings.warn('Two spectra from the same configurations were input. '
+                          'This function will not coadd them. You may want to do '
+                          'that before feeding them in here.')
+        
+        #read in all of the spectra and sort them by starting wavelength
+        spectbls = []
+        for sf in spectra:
+            spectbls.extend(fits2tbl(sf))
+            
     sortkey = lambda spec: np.min(spec['w0'])
-    spectra.sort(key=sortkey)
-    
+    spectbls.sort(key=sortkey)
+        
     #now splice them all together
-    return reduce(splice, spectra)
+    return reduce(splice, spectbls)
 
-def normalize(spectbla, spectblb, SNcut=2.0):
+def normalize(spectbla, spectblb, SNcut=2.0, method='area'):
     """
-    Normalized the spectra in specs by ensuring the median value of overlapping
-    regions are the same.
+    Normalized the spectrum b to spectrum a. 
     
     The spectra are assumed to be orded by quality. Thus, spectra[0] is the
     gold standard against which all others are normalized. Use only points with
@@ -58,7 +98,7 @@ def normalize(spectbla, spectblb, SNcut=2.0):
     normspec['error'] *= normfac
     return normspec
 
-def splice(spectbla, spectblb):
+def splice(spectbla, spectblb, normalize=False):
     """
     Combine two (presumably overlapping) spectra.
     
@@ -88,13 +128,13 @@ def splice(spectbla, spectblb):
         basegrid = np.append(w0base, w1base[-1])
         N = len(basegrid) - 1
         ospecs = [b[o] for b,o in zip(both, over)] #trim spectra to just overlap
-        ospecs[not base] = __spectbl_rebin(ospecs[not base], basegrid) #rebin the finer spectrum
+        ospecs[not base] = rebin(ospecs[not base], basegrid) #rebin the finer spectrum
         
         #now figure out where to splice the two
         def integratedSN(spectbls): #determine the cumulative signal to noise, summing bins
             np.vstack(map(np.array, spectbls))
             dw = abs(spectbls['w1'] - spectbls['w0'])
-            fw, ew = spectbl['flux']*dw, spectbl['error']*dw
+            fw, ew = spectbls['flux']*dw, spectbls['error']*dw
             return np.sum(fw, 1)/np.sqrt(np.sum(ew**2), 1)
         if wr2[1] > wr1[1]: #just the ends of the spectra overlap
             #computed integrated SN for all possible splice positions, choose the best
@@ -126,13 +166,24 @@ def splice(spectbla, spectblb):
             rightspec = both[0][globi1[0]:]
             return vstack([leftspec, midspec, rightspec])
         
-def powerbin(specfile, R=1000.0):
+def powerbin(spectbl, R=1000.0):
     """
     Rebin a spectrum onto a grid with constant resolving power.
+    
+    If the constant R grid cannot does not permit an integer number of bins
+    within the original wavelength range, the remainder will be discarded.
     """
-    pass
+    start = spectbl['w0'][0]
+    end = spectbl['w1'][-1]
+    dwmin = start/R
+    Nmax = (end-start)//dwmin + 1
+    w = np.zeros(Nmax)
+    w[0] = start
+    for i in np.arange(1,Nmax): w[i] = w[i-1]*(1 + 1/(R - 0.5))
+    w = w[w <= end]
+    return rebin(spectbl, w)
 
-def spectbl(specfile):
+def fits2tbl(specfile):
     """
     A catch-all function to read in FITS spectra from all variety of 
     instruments and provide standardized output as a list of astropy tables.
@@ -142,10 +193,10 @@ def spectbl(specfile):
     the spectral band) is used to determine how to parse the FITS file .
     
     The table has columns 'w0','w1' for the wavelength edges, 'flux', 'error',
-    'exptime', and 'instrument'. The 'instrument' column contains a number
-    identifying the instrument configuration.
+    'exptime', and 'instrument'. The 'source' column contains a number
+    identifying the instrument or model source for the data.
     
-    Instrument Identifiers
+    Source Identifiers
     ----------------------
     0 : HST COS G130M
     1 : HST COS G160M
@@ -156,16 +207,45 @@ def spectbl(specfile):
     6 : HST STIS G140M
     7 : HST STIS G230L
     8 : HST STIS G430L
-    9 : XMM-NEWTON
+    9 : XMM ---- -----
     """
-    observatory, instrument, grating = specfile[:3], specfile[4:7], specfile[8:13]
-    if observatory.upper() == 'HST':
+    sourcelist = ['HST_COS_G130M','HST_COS_G160M','HST_COS_G230L',
+                  'HST_STIS_E140M','HST_STIS_E230M','HST_STIS_E230H',
+                  'HST_STIS_G140M','HST_STIS_G230L','HST_STIS_G430L',
+                  'XMM_----_-----']
+    config = path.basename(specfile)[2:15]
+    index = sourcelist.index(config)
+    observatory = config[:3].upper()
+    
+    if observatory == 'HST':
         spec = fits.open('specfile')
+        getval = lambda s: spec[1].header[s]
+        Norders, Npts, exptime = map(getval, ['naxi2', 'naxis1', 'exptime'])
+        xnames = ['wavelength','flux','error', 'dq']
+        wmid, flux, err, flags = [spec[1].data[s] for s in xnames]
+        wedges = np.array(map(mnp.mids2edges, wmid))
+        w0, w1 = wedges[:,:-1], wedges[:,1:]
+        iarr = np.ones([Norders, Npts])*index
+        exptarr = np.array([Norders,Npts])*exptime
+        datas = np.array([w0,w1,flux,err,exptarr,flags,iarr])
+        datas.swapaxes(0,1)
+    if observatory == 'XMM':
+        pass
+    
+    cols = ['w0','w1','flux','error','exptime','flags','source']
+    dtypes = ['f4']*5 + [flags.dytpe] + ['i1']
+    spectbls = [Table(data, names=cols, dtype=dtypes) for data in datas]
+    return spectbls
         
 def coadd(spectbls):
-    pass
+    listify = lambda s: [spec[s] for spec in spectbls]
+    cols = ['w0','w1','flux','error','exptime','flags']
+    w0, w1, f, e, w, dq = map(listify, cols)
+    mask = dq > 0
+    we = [np.append(ww0,ww1[-1]) for ww0,ww1 in zip(w0,w1)]
+    return specutils.coadd(we, f, e, w, mask)
 
-def __spectbl_rebin(spec, newedges):
+def rebin(spec, newedges):
     oldedges = np.append(spec['w0'], spec['w1'][-1])
     flux, error = specutils.rebin(newedges, oldedges, spec['flux'], spec['error'])
     newspec = Table(spec, copy=True)
